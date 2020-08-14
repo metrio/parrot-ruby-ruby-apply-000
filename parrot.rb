ac5025f7de1ff52a95d9d36e16f68c661e7c8322
Demetrio Lima <dlima.nyit@gmail.com>
2020-08-14 17:51:28 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,5 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot (Phrase= "Squawk!", "Pretty Bird!")
+def parrot (Phrase = "Squawk!", )
   Puts "#{Phrase}!"
 end
